90de104fce5b9dd3adbd7e2cd4773f27490a11dd
goodcrazycreeper <esjohnson20147@gmail.com>
2025-05-02 01:16:57 -0500

    Stablized Branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import modules.universe as universe
 
 # universe.getUniverseData(124180448122765)
-#print(universe.getMethod(f'https://apis.roblox.com/universes/v1/places/{124180448122765}/universe'))
+placeid = 124180448122765
+print(universe.getMethod(f'https://apis.roblox.com/universes/v1/places/{placeid}/universe')["data"]["universeId"])
 print(universe.getUniverseData(124180448122765))
--- modules/universe.py
+++ modules/universe.py
@@ -1,34 +1,16 @@
 # Universe.py responsible for retrieving universe data such as number of subplaces.
 import requests
+import time
 from functools import wraps
 
 def check_key_exists(d, key): 
     return key in d # Check if the 'address' key exists in the dictionary
 
-def getProtector(func): # Function for get Method just incase 200 isnt returned
-     # @wraps(func)
-     def wrapper(*args, **kwargs):
-            try:
-                result = func(*args,**kwargs)
-                # if isinstance(result, tuple) and len(result) == 2: # if the return value of func is a tupel and the length is 2 
-                data, status = result
-                if status == 200:
-                    print("Success:", status)
-                elif status == 400 and "errors" in data:
-                    return data["errors"][0]
-                else:
-                    print(f"Unhandled status code: {status}")
-                return result
-                # else:
-                #     print("Unexpected return format from function.")
-                #     return result
-            except Exception as e:
-                    print("An exception occurred:", e)
-                    return {"error": str(e)}, 500
-            return wrapper
-@getProtector
 def getMethod(url,*args,**kwargs): # Return dict
-    response = requests.get(url,*args,**kwargs) # Translate placeid into universeid
+    try:
+        response = requests.get(url,*args,**kwargs) # Translate placeid into universeid
+    except:
+         return "An error occured " + response.status_code
     return {"data": response.json(), "status": response.status_code}
 
 def getUniverseData(placeId):
@@ -48,3 +30,4 @@ class Universe(): # A class named universe, which a group of places/games with o
 # https://develop.roblox.com/v2/universes/7296012067/places
 
 # BadRequest = Universe(72960120933333399999999999999967)
+
